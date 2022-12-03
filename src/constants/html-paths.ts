e9b99fdec37545162214c4fd03dc6bc4a5a34bfe
Abdurazzoq <abdurazzox9655@gmail.com>
2022-12-04 01:21:22 +0500

    [+] add Constants html paths

---
+++ src/constants/html-paths.ts
@@ -0,0 +1,13 @@
+import {IPostAutoElon} from "../interfaces/Constants";
+
+export const PathToDataPostAutoElon: IPostAutoElon = {
+    name: "body > div.site-container > div.site-container__primary > main > div > div.row > section > div > header > div.a-title > div.a-title__container > h1 > span:nth-child(2)",
+    address: "body > div.site-container > div.site-container__primary > main > div > div.row > section > div > div.clearfix.a-info-block > div.col-sm-6.description > div > dl > dd:nth-child(2)",
+    color: "body > div.site-container > div.site-container__primary > main > div > div.row > section > div > div.clearfix.a-info-block > div.col-sm-6.description > div > dl > dd:nth-child(14)",
+    engineSize: "body > div.site-container > div.site-container__primary > main > div > div.row > section > div > div.clearfix.a-info-block > div.col-sm-6.description > div > dl > dd:nth-child(6)",
+    fuelType: "body > div.site-container > div.site-container__primary > main > div > div.row > section > div > div.clearfix.a-info-block > div.col-sm-6.description > div > dl > dd:nth-child(12)",
+    mileage: "body > div.site-container > div.site-container__primary > main > div > div.row > section > div > div.clearfix.a-info-block > div.col-sm-6.description > div > dl > dd:nth-child(10)",
+    position: "body > div.site-container > div.site-container__primary > main > div > div.row > section > div > header > div.a-title > div.a-title__container > h1",
+    year: "body > div.site-container > div.site-container__primary > main > div > div.row > section > div > div.clearfix.a-info-block > div.col-sm-6.description > div > dl > dd:nth-child(4)",
+    price: "body > div.site-container > div.site-container__primary > main > div > div.row > section > div > header > div.a-price > span"
+}
---
+++ src/interfaces/Constants.ts
@@ -0,0 +1,11 @@
+export interface IPostAutoElon {
+    name: string;
+    address: string;
+    color: string;
+    engineSize: string;
+    fuelType: string;
+    mileage: string;
+    position: string;
+    year:  string;
+    price: string
+}

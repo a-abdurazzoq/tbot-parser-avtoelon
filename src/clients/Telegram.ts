1085f8c04eab57df100cb2dcad4cfd94fcd6f908
Abdurazzoq <abdurazzox9655@gmail.com>
2022-12-06 00:15:09 +0500

    Project Done

---
+++ src/clients/Telegram.ts
@@ -0,0 +1,26 @@
+import {ITelegramClient} from "../interfaces/clients/Telegram";
+import fetch from "node-fetch";
+import {IRequestDataTelegram} from "../interfaces/libs/Telegram";
+import {ITelegramBot} from "../interfaces/Constants";
+
+export default class TelegramClient implements ITelegramClient {
+    constructor(
+        private telegramBotData: ITelegramBot
+    ) {
+    }
+
+    sendDocumentsWithCaption(requestData: IRequestDataTelegram): Promise<any> {
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "chat_id": this.telegramBotData.chat_id,
+                ...requestData
+            })
+        };
+
+        return fetch(`https://api.telegram.org/bot${this.telegramBotData.token}/sendMediaGroup`, requestOptions)
+    }
+}

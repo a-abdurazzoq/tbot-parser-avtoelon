ee274ac5dcf5dddbb05efcb4df424661b46e0302
Abdurazzoq <abdurazzox9655@gmail.com>
2022-12-27 00:35:10 +0500

    [+] Add property description

--- src/constants/html-paths.ts
+++ src/constants/html-paths.ts
@@ -2,6 +2,7 @@ import {IKeyToPostAutoElon, IPathToPostAutoElon} from "../interfaces/Constants";
 
 export const PathToDataPostAutoElon: IPathToPostAutoElon = {
     name: "header div.a-title div.a-title__container h1",
+    description: ".description .description-text",
     position: "header div.a-title div.a-title__container h1",
     price: ".a-price__text",
     images: ".photos-block > .photo-list a",
